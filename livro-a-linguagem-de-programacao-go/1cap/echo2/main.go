e21f004cd59ff5600cc2e6f8e0a708af4fa6251e
Dayana Maia <dayrmaia@gmail.com>
2018-02-06 13:34:34 -0200

    exercicios cap1

---
+++ livro-a-linguagem-de-programacao-go/1cap/echo2/main.go
@@ -0,0 +1,16 @@
+// echo2 exibe seus argumentos de linha de comando
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	var s, sep string
+	for _, arg := range os.Args[1:] {
+		s += sep + arg
+		sep = " "
+	}
+	fmt.Println("comando executado:", s)
+}
